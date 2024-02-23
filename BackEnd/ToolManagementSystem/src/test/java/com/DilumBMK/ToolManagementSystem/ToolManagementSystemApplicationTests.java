b827eb63368254ec2daad7d9eb9128ec98105619
NilmiSenevirathne <126900940+NilmiSenevirathne@users.noreply.github.com>
2024-02-23 10:58:10 +0530

    Database creation
    Create some tables in the BMK database

---
+++ BackEnd/ToolManagementSystem/src/test/java/com/DilumBMK/ToolManagementSystem/ToolManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DilumBMK.ToolManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
